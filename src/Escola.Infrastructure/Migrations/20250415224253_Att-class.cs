244ffc820142a69d81c1a9f424618431f58e0256
guxttavo <gustavowbarbosas@gmail.com>
2025-04-15 19:46:11 -0300

    script de insert atualizado

---
+++ src/Escola.Infrastructure/Migrations/20250415224253_Att-class.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Enceja.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Attclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "class");
+
+            migrationBuilder.RenameColumn(
+                name: "period",
+                table: "class",
+                newName: "shift");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "shift",
+                table: "class",
+                newName: "period");
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "class",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
