273b994a00ccd93fb71eaa1698c5a13e96441405
guxttavo <gustavowbarbosas@gmail.com>
2025-04-15 19:27:44 -0300

    atualizacao nos atributos de class

--- src/Escola.Domain/Entities/Class.cs
+++ src/Escola.Domain/Entities/Class.cs
@@ -3,17 +3,32 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Enceja.Domain.Entities
 {
+    public enum Periodo
+    {
+        Manha = 1,
+        Tarde = 2,
+        Noite = 3
+    }
+    
     [Table("class")]
     public class Class
     {
         [Column("id")]
         public int Id { get; set; }
 
+        [Column("year")]
+        public int Ano { get; set; }
+
+        [Column("period")]
+        public Periodo Periodo { get; set; }
+
+         [Column("suffix")]
+        public string Suffix { get; set; }
+
         [Column("name")]
         public string Name { get; set; }
 
         public ICollection<Student>? Students { get; set; }
         public ICollection<Teacher_Class> Teachers_Class { get; set; }
-
     }
 }
---
+++ src/Escola.Infrastructure/Migrations/20250415222551_atualizacao-class.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Enceja.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class atualizacaoclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "period",
+                table: "class",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "suffix",
+                table: "class",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "year",
+                table: "class",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "period",
+                table: "class");
+
+            migrationBuilder.DropColumn(
+                name: "suffix",
+                table: "class");
+
+            migrationBuilder.DropColumn(
+                name: "year",
+                table: "class");
+        }
+    }
+}
