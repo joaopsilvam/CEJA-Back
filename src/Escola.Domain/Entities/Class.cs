eea5beb32552aaed4d045a17e4b994427d8ab1c5
guxttavo <gustavowbarbosas@gmail.com>
2025-04-15 19:38:04 -0300

    correções class

--- src/Escola.Domain/Entities/Class.cs
+++ src/Escola.Domain/Entities/Class.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Enceja.Domain.Entities
 {
-    public enum Periodo
+    public enum Shift
     {
         Manha = 1,
         Tarde = 2,
@@ -17,16 +17,15 @@ namespace Enceja.Domain.Entities
         public int Id { get; set; }
 
         [Column("year")]
-        public int Ano { get; set; }
+        public int Year { get; set; }
 
-        [Column("period")]
-        public Periodo Periodo { get; set; }
+        [Column("shift")]
+        public Shift shift { get; set; }
 
          [Column("suffix")]
         public string Suffix { get; set; }
 
-        [Column("name")]
-        public string Name { get; set; }
+        
 
         public ICollection<Student>? Students { get; set; }
         public ICollection<Teacher_Class> Teachers_Class { get; set; }
